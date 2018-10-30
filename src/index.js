95fdde1fda683fe6c7e1ea50dee6761b67ea59d2
Thulio Philipe <thulioph@gmail.com>
2018-10-30 18:32:15 -0300

    Updates the main file.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
 const sample = require('../api/sample');
 const system = require('../api/system');
 
+const _saveFile = ({ values }) => {
+  system.writeFile({
+    name: `files/${artistName}.json`,
+    data: JSON.stringify(values),
+  });
+}
+
+const _handleSuccess = (result) => {
+  if (result && result.length > 0) {
+    const info = result.map(({ sampleUrl }) => sample.getInfo(sampleUrl));
+    Promise.all(info).then(values => _saveFile(values));
+  }
+}
+
+const _handleError = (error) => {
+  console.warn('Algo deu errado:', error);
+}
+
 const artist = process.argv[2] || null;
+let artistName;
 
 if (artist) {
-  const artistName = artist.toLowerCase().replace(' ', '-');
+  artistName = artist.toLowerCase().replace(' ', '-');
+
   const songs = sample.songs(artist);
-
-  songs.then(result => {
-    if (result && result.length > 0) {
-      const info = result.map(({ sampleUrl }) => sample.getInfo(sampleUrl));
-
-      Promise.all(info).then(values => {
-        system.writeFile({
-          name: `${artistName}.json`,
-          data: JSON.stringify(values),
-        });
-      });
-    }
-  }).catch(err => {
-    console.warn('Algo deu errado:', err);
-  });
+  songs.then(_handleSuccess).catch(_handleError);
 }
