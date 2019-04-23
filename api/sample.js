a517e7ddde0b200ba6111d3776472b36c6194a36
Thulio Philipe <thulioph@gmail.com>
2019-04-23 15:11:18 -0300

    Updates the sample file.

--- api/sample.js
+++ api/sample.js
@@ -4,6 +4,32 @@ const config = {
   API_URL: 'http://www.whosampled.com/search',
 };
 
+const sampleInfo = {
+  metaData: {
+    kind: '.section-header .section-header-title | trim'
+  },
+  dest: {
+    name: '#sampleWrap_dest  .trackName | trim',
+    artist: '#sampleWrap_dest .sampleTrackArtists | trim',
+    release: '#sampleWrap_dest .release-name a',
+    year: '#sampleWrap_dest .trackLabel span + span',
+    label: '#sampleWrap_dest .trackLabel span',
+    image: '#sampleWrap_dest .sampleTrackImage img@src',
+    appearsAt: '.dest-timing',
+    youtubeId: '.embed-dest iframe@src | parseYoutubeId'
+  },
+  source: {
+    name: '#sampleWrap_source  .trackName | trim',
+    artist: '#sampleWrap_source .sampleTrackArtists | trim',
+    release: '#sampleWrap_source .release-name a',
+    year: '#sampleWrap_source .trackLabel span + span',
+    label: '#sampleWrap_source .trackLabel span',
+    image: '#sampleWrap_source .sampleTrackImage img@src',
+    appearsAt: '.source-timing',
+    youtubeId: '.embed-source iframe@src | parseYoutubeId'
+  }
+};
+
 const sample = {
   songs(query) {
     const { API_URL } = config;
@@ -21,31 +47,7 @@ const sample = {
 
   getInfo(url) {
     return new Promise((resolve, reject) => {
-      request(url, {
-        metaData: {
-          kind: '.section-header .section-header-title | trim'
-        },
-        dest: {
-          name: '#sampleWrap_dest  .trackName | trim',
-          artist: '#sampleWrap_dest .sampleTrackArtists | trim',
-          release: '#sampleWrap_dest .release-name a',
-          year: '#sampleWrap_dest .trackLabel span + span',
-          label: '#sampleWrap_dest .trackLabel span',
-          image: '#sampleWrap_dest .sampleTrackImage img@src',
-          appearsAt: '.dest-timing',
-          youtubeId: '.embed-dest iframe@src | parseYoutubeId'
-        },
-        source: {
-          name: '#sampleWrap_source  .trackName | trim',
-          artist: '#sampleWrap_source .sampleTrackArtists | trim',
-          release: '#sampleWrap_source .release-name a',
-          year: '#sampleWrap_source .trackLabel span + span',
-          label: '#sampleWrap_source .trackLabel span',
-          image: '#sampleWrap_source .sampleTrackImage img@src',
-          appearsAt: '.source-timing',
-          youtubeId: '.embed-source iframe@src | parseYoutubeId'
-        }
-      })((err, result) => {
+      request(url, sampleInfo)((err, result) => {
         if (err) reject(err);
         resolve(result);
       });
