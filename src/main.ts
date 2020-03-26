b97679077e6d8e05738cba34bda2a4d211096bce
Thulio Philipe <thulioph@gmail.com>
2020-03-26 18:26:55 -0300

    feat(nestjs): add helmet to prevent security vulnerabilities

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import * as helmet from 'helmet';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   await app.listen(3000);
 }
 bootstrap();
