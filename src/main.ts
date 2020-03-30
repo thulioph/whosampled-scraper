adf02b6390503a052860ada4802122226c1f89e4
Thulio Philipe <thulioph@gmail.com>
2020-03-30 19:24:31 -0300

    chore(heroku): get any available port

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
