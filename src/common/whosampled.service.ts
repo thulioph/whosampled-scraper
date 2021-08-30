d5685b4643e7de3d92068d714f9279c7a8cf2694
spencer cap <spencercap@users.noreply.github.com>
2021-08-30 20:56:17 +0100

    fix(req): added x-ray driver for custom User-Agent header (#12)
    fakes firefox User-Agent header

--- src/common/whosampled.service.ts
+++ src/common/whosampled.service.ts
@@ -1,11 +1,22 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import * as xray from 'x-ray';
 
-import { Nodes } from './nodes.service'
+// driver
+import * as makeDriver from 'request-x-ray';
+const options = {
+  method: 'GET', // Set HTTP method
+  jar: true, // Enable cookies
+  headers: {
+    'User-Agent': 'Firefox/48.0', // Set headers
+  },
+};
+const driver = makeDriver(options); //Create driver
+
+import { Nodes } from './nodes.service';
 
 @Injectable()
 export class WhoSampledService {
-  private baseUrl: string = 'https://www.whosampled.com/search';
+  private baseUrl = 'https://www.whosampled.com/search';
 
   private getConnectionsUrl(artistName: string): string {
     return `${this.baseUrl}/connections/?q=${encodeURIComponent(artistName)}`;
@@ -18,11 +29,16 @@ export class WhoSampledService {
   ): Promise<[]> {
     if (!url || !selector || !scope) throw new Error('Missing parameters.');
     const x = xray();
+    x.driver(driver);
     return await x(url, selector, scope);
   }
 
   async fetchConnectionsDetails(URL: string): Promise<[]> {
-    const details = await this.makeXrayRequest(URL, 'body', Nodes.detailedEntry());
+    const details = await this.makeXrayRequest(
+      URL,
+      'body',
+      Nodes.detailedEntry(),
+    );
 
     if (!details) {
       throw new NotFoundException('No connections found for this artist.');
@@ -33,7 +49,11 @@ export class WhoSampledService {
 
   private async fetchArtistConnections(artistName: string): Promise<[]> {
     const URL = this.getConnectionsUrl(artistName);
-    const connections = await this.makeXrayRequest(URL, 'li.listEntry', Nodes.baseEntry());
+    const connections = await this.makeXrayRequest(
+      URL,
+      'li.listEntry',
+      Nodes.baseEntry(),
+    );
 
     if (!connections || !connections.length) {
       throw new NotFoundException('No connections found for this artist.');
@@ -48,7 +68,7 @@ export class WhoSampledService {
     const connections = await this.fetchArtistConnections(artistName);
 
     const info = connections.map(
-      async ({ link }) => await this.fetchConnectionsDetails(link)
+      async ({ link }) => await this.fetchConnectionsDetails(link),
     );
 
     return await Promise.all(info).then(data => data);
